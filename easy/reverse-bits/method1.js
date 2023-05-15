2adedcdd3b68ce135fb2b82dc5f3991d1bb8f4e1
RunDay198 <runday198@gmail.com>
2023-05-15 14:02:49 +0400

    reverse-bits

--- easy/reverse-bits/method1.js
+++ easy/reverse-bits/method1.js
@@ -1,3 +1,14 @@
-function reverseBits(n) {}
+function reverseBits(n) {
+  var result = 0;
+  var count = 32;
+
+  while (count--) {
+    result *= 2;
+    result += n & 1;
+    n = n >> 1;
+  }
+
+  return result;
+}
 
 reverseBits();
