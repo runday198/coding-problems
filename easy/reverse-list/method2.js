7d3b90d0e6d90d3b58693ab04c5dd8b3249dc355
RunDay198 <runday198@gmail.com>
2023-05-16 12:34:13 +0400

    easy/reverse-list

--- easy/reverse-list/method2.js
+++ easy/reverse-list/method2.js
@@ -1,3 +1,17 @@
-function reverseList(head) {}
+function reverseList(head) {
+  return recursive(head);
+}
 
-function recursive() {}
+function recursive(head, prev = null, curr = null) {
+  if (!head) {
+    return curr;
+  }
+
+  curr = head;
+  head = head.next;
+
+  curr.next = prev;
+  prev = curr;
+
+  return recursive(head, prev, curr);
+}
