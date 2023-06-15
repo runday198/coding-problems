bb16beaeb599bb84abf8091793ca16835820f818
RunDay198 <runday198@gmail.com>
2023-06-15 14:53:32 +0400

    medium/valid-bst

---
+++ medium/valid-bst/method.js
@@ -0,0 +1,29 @@
+var isValidBST = function (root) {
+  var left = bfs(root.left, -Number.MAX_VALUE, root.val);
+  var right = bfs(root.right, root.val, Number.MAX_VALUE);
+
+  return left && right;
+};
+
+function bfs(node, min, max) {
+  if (!node) {
+    return true;
+  }
+
+  console.log(node.val, min, max);
+
+  if (node.val >= max || node.val <= min) {
+    console.log("1");
+    return false;
+  }
+
+  var left = bfs(node.left, min, node.val);
+  var right = bfs(node.right, node.val, max);
+
+  if (!left || !right) {
+    console.log("2");
+    return false;
+  }
+
+  return true;
+}
